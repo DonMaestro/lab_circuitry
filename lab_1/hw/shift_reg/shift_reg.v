23872e61decf0e1ff48f850957d7049613fc3169
unknown <vitalik16200@gmail.com>
2020-10-07 23:40:39 +0300

    fm

---
+++ lab_1/hw/shift_reg/shift_reg.v
@@ -0,0 +1,23 @@
+module shift_reg (CLOCK_50, KEY, SW, LEDR);
+
+input				CLOCK_50;
+input   [1:0]   KEY;
+input   [9:0]   SW;
+output  [9:0]   LEDR;
+
+wire            clk = KEY[0];
+wire            rst_n = KEY[1];
+
+reg     [9:0]   shift_reg;
+
+assign LEDR = shift_reg;
+                                        
+always @(posedge clk, negedge rst_n)
+
+    if (~rst_n) begin
+        shift_reg <= 0;
+    end else begin
+        shift_reg <= {shift_reg[8:0], SW[0]};    
+    end
+    
+endmodule
