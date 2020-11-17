14b4e4979a6bd8a4ac744ee79210a2a52275a04d
unknown <vitalik16200@gmail.com>
2020-11-17 21:32:12 +0200

    виправив nco

--- lab3/hw/fm_transmitter/fm_transmitter.v
+++ lab3/hw/fm_transmitter/fm_transmitter.v
@@ -1,7 +1,7 @@
-module fm_transmitter(CLOCK_50, GPIO, KEY, LEDR);
+module fm_transmitter(CLOCK_50, GPIO_0, KEY, LEDR);
 
 input           CLOCK_50;
-inout   [35:0]  GPIO;
+inout   [35:0]  GPIO_0;
 input   [1:0]   KEY;
 output  [9:0]   LEDR;
 
@@ -18,14 +18,14 @@ reg     [31:0]  nco_phase_step;
 wire    [31:0]  nco_phase_step_carrier = NCO_PHASE_STEP_CARRIER;
 wire    [31:0]  nco_phase_step_deviation = { {(32-8-13){sample[7]}}, sample, 13'b0 };
 
-mypll pll_inst(.inclk0 (CLOCK_50),
+my_pll pll_inst(.inclk0 (CLOCK_50),
                 .c0 (sys_clk), // 250 MHz clock
                 .locked (LEDR[0])
                 );
     
 simple_uart_receiver uart_rec(.i_clk (sys_clk), 
                                 .i_rst_n (sys_rst_n), 
-                                .i_rx (GPIO[0]), 
+                                .i_rx (GPIO_0[0]), 
                                 .o_dat (uart_dat), 
                                 .o_dat_vld ()
                                 );
@@ -39,6 +39,6 @@ always @(posedge sys_clk)
 always @(posedge sys_clk)
     nco_phase <= nco_phase + nco_phase_step;
 
-assign GPIO[35] = nco_phase[31];
+assign GPIO_0[35] = nco_phase[31];
                                 
 endmodule
